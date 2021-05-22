e0ef1657884e101ff985ddc53f8031a687971755
crystal <noelani.villanueva@gmail.com>
2021-05-22 12:10:56 -0700

    portfolio name and font

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./NavBar";
+import HomeLayout from "./HomeLayout";
 
 function Home() {
   return (
     <div>
       <NavBar />
+      <HomeLayout />
     </div>
   );
 }
---
+++ src/HomeLayout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomeLayout() {
+  return <div className="nav-name">Crystal Villanueva Portfolio</div>;
+}
+
+export default HomeLayout;
