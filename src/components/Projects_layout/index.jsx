50cc93eb1f512a83f9eedc0bf1e8c801f005efb8
crystal <noelani.villanueva@gmail.com>
2021-06-25 12:25:42 -0700

    portfolio

--- src/components/Projects_layout/index.jsx
+++ src/components/Projects_layout/index.jsx
@@ -1,40 +1,28 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import GridListTileBar from "@material-ui/core/GridListTileBar";
-import ListSubheader from "@material-ui/core/ListSubheader";
+import Tilt from "react-vanilla-tilt";
 import projects from "../../projects.js";
 import project from "./projectstyle.css";
 import { Link } from "react-router-dom";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    // justifyContent: "space-around",
-    overflow: "hidden",
-    backgroundColor: theme.palette.background.paper,
-  },
-  icon: {
-    color: "rgba(255, 255, 255, 0.54)",
-  },
-  gridList: {
-    width: 500,
-    height: 450,
-    padding: "20px",
-  },
-}));
-
 function index() {
   return (
-    <div>
-      <Link to="/" className="backbtn2">
-        <button>back</button>
-      </Link>
-      {projects.map((x) => (
-        <div>{x.name}</div>
-      ))}
+    <div className="backGround">
+      <div className="glass">
+        <div className="parent">
+          <Link to="/" className="linkStyle">
+            <li className="homeStyle2">Home</li>
+          </Link>
+
+          <div className="projectLayout">
+            {projects.map((x) => (
+              <div className="item">
+                <img src={x.image} />
+                <div className="projectName">{x.name} </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
